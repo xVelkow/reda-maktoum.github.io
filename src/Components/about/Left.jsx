d583715ef7d6d27e66296736e27f74794d343011
xVelkow <redamaktoum@gmail.com>
2023-07-22 19:53:03 +0100

    copywriting solved

--- src/Components/about/Left.jsx
+++ src/Components/about/Left.jsx
@@ -4,7 +4,7 @@ const Left = () =>{
             <h1>Who am I?</h1>
             <section>
                 <p>Hello! my name is <span>Reda Maktoum</span>, <br/>
-                I graduated from the Specialized Institute <br className="desktop-tablet" /> of Applied Technoilogy, <br className="mobile"/>
+                I graduated from the Specialized Institute <br className="desktop-tablet" /> of Applied Technology, <br className="mobile"/>
                 and I became specialist<br className="desktop-tablet" /> in both <span>front-end</span> and <span>back-end</span><br className="mobile"/> Web Development.</p>
 
                 <p>I enjoy <span>creating things</span> that live on the internet,<br className="desktop-tablet"/> Therefore
